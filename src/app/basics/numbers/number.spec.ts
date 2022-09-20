47efaffe7c17b08c42037df1a8f25aa6b2826cb8
rafikidota <lesmes240698@gmail.com>
2022-09-20 16:04:52 -0400

    Update number.spec.ts

--- src/app/basics/numbers/number.spec.ts
+++ src/app/basics/numbers/number.spec.ts
@@ -1,11 +1,11 @@
 import { increase } from "./number";
 
 describe('Number tests', () => {
-    it('it has to return 100 if number is major than 100', () => {
+    it('it has to return 100 if number is greater than 100', () => {
         const res = increase(300);
         expect(res).toBe(100);
     });
-    it('it has to return number + 1 if it is minor than 100', () => {
+    it('it has to return number + 1 if it is less than 100', () => {
         const res = increase(50);
         expect(res).toBe(51);
     });
