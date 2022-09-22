85be09aedcfd4a9b8f9b48982189fc6815ac2482
rafikidota <lesmes240698@gmail.com>
2022-09-22 14:30:58 -0400

    hospital component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorsComponent } from './intermediates/spies/doctor.component';
 import { DoctorComponent } from './intermediates_integration/doctor/doctor.component';
+import { HospitalComponent } from './intermediates_integration/hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DoctorsComponent,
-    DoctorComponent
+    DoctorComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule,
